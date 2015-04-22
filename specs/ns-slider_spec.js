d50941a0262a7264329b8e13888d4027affcb476
Climbus <climbus@gmail.com>
2015-04-22 15:57:12 +0200

    next

--- specs/ns-slider_spec.js
+++ specs/ns-slider_spec.js
@@ -33,5 +33,50 @@ module.exports = function(driver) {
               done();
         });
   	});
+
+  	it("should has buttons", function (done) {
+  		driver.findElements(webdriver.By.xpath("//ns-slider//span[@id='next'] | //ns-slider//span[@id='prev']"))
+          .then(function(elms) {
+              expect(elms.length).toBe(2);
+              done();
+        });
+  	});
+
+  	it("should show only one image", function (done) {
+  		function visibleImgs(element) {
+		  var imgs = element.findElements(webdriver.By.xpath("//ns-slider//img"));
+		  return webdriver.promise.filter(imgs, function(img) {
+		    return img.isDisplayed();
+		  }).then(function(visibleImgs) {
+		    return visibleImgs;
+		  });
+		}
+
+  		driver.findElements(visibleImgs)
+          .then(function(elms) {
+          	  expect(elms.length).toBe(1);
+          	  done();
+        });
+  	});
+  	it("should show next image on next", function (done) {
+  		driver.findElement(webdriver.By.xpath("//ns-slider//img[contains(@src, '1.jpg')]")).isDisplayed()
+  			.then(function(present) {
+  				expect(present).toBe(true);
+  				driver.findElement(webdriver.By.xpath("//ns-slider//img[contains(@src, '2.jpg')]")).isDisplayed()
+  					.then(function(present) {
+  						expect(present).toBe(false);
+  						driver.findElement(webdriver.By.xpath("//ns-slider//*[@id='next']")).click();
+  						driver.findElement(webdriver.By.xpath("//ns-slider//img[contains(@src, '1.jpg')]")).isDisplayed()
+				  			.then(function(present) {
+				  				expect(present).toBe(false);
+				  				driver.findElement(webdriver.By.xpath("//ns-slider//img[contains(@src, '2.jpg')]")).isDisplayed()
+				  					.then(function(present) {
+				  						expect(present).toBe(true);
+				  						done();
+				  					});
+				  			});
+					});
+  			});
+	});
   });
 }
